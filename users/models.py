3f94b5b93f2a54baae353638ade22aef2959f24a
ArtyomViryutin <virytinart1@gmail.com>
2021-01-13 00:26:49 +0300

    3rd commit

--- users/models.py
+++ users/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 from django.views.generic.edit import FormView
-# from django.contrib.auth.mixins import
-# Create your models here.
 
-from django.contrib.auth.mixins import Pa
-
-# class PasswordResetView(PasswordContextMixin, FormView):
-#     pass
